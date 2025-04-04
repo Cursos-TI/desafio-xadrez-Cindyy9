468845282b1c23566c324834c661325921783c2d
ByteMe <cindyarkan7@gmail.com>
2025-04-04 22:11:36 +0000

    Adicionei o cavalo e o coloquei para que desse para escolher a direção que vai

--- xadrez.c
+++ xadrez.c
@@ -2,33 +2,54 @@
 
 int main() {
      
-    int i = 0;
+    int i = 0, movimentacao = 2;
+    char direcao;
 
     printf("### Xadrez ###\n");
 
     printf("\nTorre\n");
     //Mover a torre 5 casas para direita
-    for (int i = 0; i <= 5; i++)
+    for (i = 0 ; i <= 5; i++)
     {
         printf("Direita\n");
     }
-
+    i = 1;
     printf("\nBispo\n");
+
     //Mover o bispo 5 casas para diagonal direita
     while (i <= 5)
     {
         printf("Cima, direita\n");
         i++;
     }
-
+    
+    i = 1;
     printf("\nRainha\n");
     //Mover 8 casas para a esquerda
-    do
-    {
+    do {
         printf("Esquerda\n");
         i++;
     } while (i <= 8);
-    
+   
+   
+    printf("Escolha a direção do cavalo (D para Direita, E para Esquerda): ");
+    scanf(" %c", &direcao);
+
+    // Garantir que a entrada seja válida
+    while (direcao != 'D' && direcao != 'E') {
+        printf("Entrada inválida! Escolha D para Direita ou E para Esquerda: ");
+        scanf(" %c", &direcao);
+    }
+
+    // Movimento do cavalo
+    while (movimentacao--) {
+        printf("Cima\n");
+    }
+    if (direcao == 'D', 'e') {
+        printf("Direita\n");
+    } else {
+        printf("Esquerda\n");
+    }
 
     return 0;
 }
